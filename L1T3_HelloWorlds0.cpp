cd0ab4faddc2bcdf90f4d14d6054ebc207308cbd
Petter Johansson <74024293+johpeq-0@users.noreply.github.com>
2020-11-15 22:57:04 +0100

    Add files via upload

---
+++ L1T3_HelloWorlds0.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    cin.get();  /* This makes the program pause. A recommended alternative to
+                System("pause"). */
+    return 0;
+}
